2e04f94eaffc540a16bd2028e6c221802ecd4732
Fabian Leiva <123709798+fabianleiva@users.noreply.github.com>
2024-03-17 16:22:49 -0300

    add comments

--- src/v1/routes/jewelryRoutes.ts
+++ src/v1/routes/jewelryRoutes.ts
@@ -1,9 +1,15 @@
 import express from "express";
 import * as jewelryController from "../controllers/jewelryControllers.js";
+
 const route = express.Router();
 
+// Ruta para obtener todas las joyas
 route.get("/", jewelryController.getJewelry);
+
+// Ruta para obtener joyas filtradas
 route.get("/filtros", jewelryController.getFilteredJewelry);
+
+// Ruta para obtener una joya por su ID
 route.get("/joya/:id", jewelryController.getOneJewelry);
 
 export default route;
