78ce76f91697d833b49b841918899a0363c11091
Fabian Leiva <123709798+fabianleiva@users.noreply.github.com>
2024-03-17 16:23:25 -0300

    add coments, create function to check connection to db

--- src/v1/config/database.ts
+++ src/v1/config/database.ts
@@ -2,6 +2,8 @@ import pg from "pg";
 import "dotenv/config";
 const { Pool } = pg;
 
+
+// Crear un nuevo grupo de conexiones (pool) para interactuar con la base de datos
 const pool = new Pool({
   host: process.env.HOST,
   user: process.env.USER,
@@ -9,5 +11,20 @@ const pool = new Pool({
   database: process.env.DATABASE,
   allowExitOnIdle: true,
 });
+
+// Funcion para checkear coneccion a la db
+const connectToDatabase = async () => {
+  try {
+    const client = await pool.connect();
+    const res = await client.query("SELECT NOW()");
+    console.log("DB CONNECTED. Current time:", res.rows[0].now);
+    client.release(); // Liberar cliente despues de su uso
+  } catch (error: any) {
+    console.error("Error connecting to the database:", error.message);
+    throw error;
+  }
+};
+
+connectToDatabase();
 
 export default pool;
